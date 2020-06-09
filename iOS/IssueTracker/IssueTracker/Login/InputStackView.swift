931ed0d6282925b8aeb530c6b423dbfb27ad09f2
TTOzzi <devttozzi@gmail.com>
2020-06-09 14:50:11 +0900

    [iOS] feat: InputStackView 커스텀 클래스 생성
    - titleLabel 과 textField 를 포함함
    - 코드로 UI 작성
    
    close #7

---
+++ iOS/IssueTracker/IssueTracker/Login/InputStackView.swift
@@ -0,0 +1,48 @@
+//
+//  InputStackView.swift
+//  IssueTracker
+//
+//  Created by TTOzzi on 2020/06/09.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+final class InputStackView: UIStackView {
+    
+    // MARK: - Properties
+    private var titleLabel: UILabel!
+    private var inputTextField: UITextField!
+    
+    // MARK: - LifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+    }
+    
+    // MARK: - Methods
+    // MARK: SetUp
+    private func setUp() {
+        axis = .vertical
+        spacing = 5
+        distribution = .fill
+        setUpTitleLabel()
+        setUpInputTextField()
+    }
+    
+    private func setUpTitleLabel() {
+        titleLabel = UILabel()
+        titleLabel.font = .boldSystemFont(ofSize: 12)
+        addArrangedSubview(titleLabel)
+    }
+    
+    private func setUpInputTextField() {
+        inputTextField = UITextField()
+        addArrangedSubview(inputTextField)
+    }
+}

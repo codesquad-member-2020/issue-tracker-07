9f3813878fd7913d9f3e420e00b3fa87a5c1a7cf
1Consumption <hanseop95@gmail.com>
2020-06-09 13:10:39 +0900

    [iOS] feat: 닫기 버튼 추가
    - 닫기 버튼을 누르면 warningView를 hidden
    
    close #6

--- iOS/IssueTracker/IssueTracker/LoginViewController.swift
+++ iOS/IssueTracker/IssueTracker/LoginViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 final class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var warningView: BorderView!
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func warningCloseButtonTapped(_ sender: UIButton) {
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0, animations: {
+            self.warningView.alpha = 0
+        }, completion: { _ in
+            self.warningView.isHidden = true
+        })
     }
 }
 

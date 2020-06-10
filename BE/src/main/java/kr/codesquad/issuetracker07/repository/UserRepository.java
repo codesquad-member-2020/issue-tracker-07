e3ff552be1dc0445928e877ccd5dd29878f85ad5
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-06-11 01:11:02 +0900

    [BE] feat: Repository 함수 추가
    - 이름과 AuthProvider로 유저 찾는 함수 추가
    
    #24

--- BE/src/main/java/kr/codesquad/issuetracker07/repository/UserRepository.java
+++ BE/src/main/java/kr/codesquad/issuetracker07/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package kr.codesquad.issuetracker07.repository;
 
+import kr.codesquad.issuetracker07.domain.AuthProvider;
 import kr.codesquad.issuetracker07.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-    Optional<User> findByEmail(String email);
+    Optional<User> findByName(String name);
+
+    Optional<User> findByNameAndAuthProvider(String name, AuthProvider authProvider);
 }

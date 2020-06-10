3d54ea6e61438367eb74fcb3216f15565ad8530d
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-06-11 01:09:09 +0900

    [BE] feat: Signup and Login 로직 서비스에 추가
    - 유저 저장 및 생성 함수 추가
    - 유저 중복 체크 함수 추가
    - 요청한 id, password가 유효한지 검사 하는 함수 추가
    
    #24

--- BE/src/main/java/kr/codesquad/issuetracker07/service/UserService.java
+++ BE/src/main/java/kr/codesquad/issuetracker07/service/UserService.java
@@ -1,5 +1,6 @@
 package kr.codesquad.issuetracker07.service;
 
+import kr.codesquad.issuetracker07.domain.AuthProvider;
 import kr.codesquad.issuetracker07.domain.User;
 import kr.codesquad.issuetracker07.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void saveUser(User user) {
+    public void saveUser(User user, AuthProvider authProvider) {
+        user.setAuthProvider(authProvider);
         userRepository.save(user);
+    }
+
+    public User makeUser(String id, String password) {
+        return User.builder()
+                   .name(id)
+                   .password(password)
+                   .avatarUrl("https://codesquad-mocha.s3.ap-northeast-2.amazonaws.com/github.png")
+                   .authProvider(AuthProvider.local)
+                   .build();
+    }
+
+    public boolean isExistedUser(String id) {
+        return userRepository.findByName(id).isPresent();
+    }
+
+    public boolean isValidIdAndPassword(String id, String password, AuthProvider authProvider) {
+        return userRepository.findByNameAndAuthProvider(id, authProvider)
+                             .filter(user -> password.equals(user.getPassword()))
+                             .isPresent();
     }
 }

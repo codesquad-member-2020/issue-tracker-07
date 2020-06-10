cfa8b653543fe7fc7eb14a61aac175a8d9fd1086
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-06-11 01:14:07 +0900

    [BE] feat: LoginVO 추가
    - 사용자가 id, password를 body에 요청 할 때 받는 VO 추가
    
    #24

---
+++ BE/src/main/java/kr/codesquad/issuetracker07/dto/LoginVO.java
@@ -0,0 +1,13 @@
+package kr.codesquad.issuetracker07.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginVO {
+
+    private String id;
+
+    private String password;
+}

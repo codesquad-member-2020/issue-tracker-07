1e390b125cc538809fbdbcd91ed7db1325ce0b5f
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-06-11 01:06:03 +0900

    [BE] feat: Signup and Login 컨트롤러에 추가
    - signup post mapping 추가
    - login post mapping 추가
    
    #24

--- BE/src/main/java/kr/codesquad/issuetracker07/controller/LoginController.java
+++ BE/src/main/java/kr/codesquad/issuetracker07/controller/LoginController.java
@@ -1,12 +1,14 @@
 package kr.codesquad.issuetracker07.controller;
 
+import kr.codesquad.issuetracker07.domain.AuthProvider;
 import kr.codesquad.issuetracker07.domain.User;
+import kr.codesquad.issuetracker07.dto.LoginVO;
 import kr.codesquad.issuetracker07.service.AuthService;
 import kr.codesquad.issuetracker07.service.JwtService;
 import kr.codesquad.issuetracker07.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,6 +28,27 @@ public class LoginController {
         this.userService = userService;
     }
 
+    @PostMapping("/signup")
+    public ResponseEntity<HttpStatus> signInNewUser(@RequestBody LoginVO loginVO) {
+        if (userService.isExistedUser(loginVO.getId())) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        userService.saveUser(userService.makeUser(loginVO.getId(), loginVO.getPassword()), AuthProvider.local);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<HttpStatus> loginLocal(@RequestBody LoginVO loginVO, HttpServletResponse response) {
+        if (userService.isValidIdAndPassword(loginVO.getId(), loginVO.getPassword(), AuthProvider.local)) {
+            String jwtToken = jwtService.makeJwtToken(loginVO.getId());
+            response.setHeader("Authorization", jwtToken);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
     @GetMapping("/login/github")
     public void loginGithub(HttpServletResponse response) throws IOException {
         response.sendRedirect(authService.createGithubAuthTokenUri());
@@ -35,8 +58,8 @@ public class LoginController {
     public void githubCallback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         String githubAccessToken = authService.getGithubAccessToken(code);
         User user = authService.getUserInformationFromToken(githubAccessToken);
-        userService.saveUser(user);
-        String jwtToken = jwtService.makeJwtToken(user.getEmail());
+        userService.saveUser(user, AuthProvider.github);
+        String jwtToken = jwtService.makeJwtToken(user.getName());
         response.sendRedirect("io.issuetracker.app://auth?token=" + jwtToken);
     }
 }

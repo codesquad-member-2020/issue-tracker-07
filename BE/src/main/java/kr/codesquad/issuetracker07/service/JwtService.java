3c8f1fc40946838673d7cd547735d98358fd25eb
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-06-10 15:44:46 +0900

    [BE] feat: JwtService 추가
    - Jwt Token 관련 로직을 처리하는 Service 추가
    
    #19

---
+++ BE/src/main/java/kr/codesquad/issuetracker07/service/JwtService.java
@@ -0,0 +1,33 @@
+package kr.codesquad.issuetracker07.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    @Value("${JWT_SECRET_KEY}")
+    private String JWT_SECRET_KEY;
+
+    public String makeJwtToken(String userEmail) {
+        final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+        final String TYP = "typ";
+        final String ALG = "HS256";
+        Map<String, Object> header = new HashMap<>();
+        header.put(TYP, "JWT");
+        header.put(ALG, "HS256");
+        Map < String, Object > payload = new HashMap<>();
+        payload.put("USER_EMAIL", userEmail);
+        return Jwts.builder()
+                   .setHeader(header)
+                   .setClaims(payload)
+                   .signWith(SIGNATURE_ALGORITHM, JWT_SECRET_KEY.getBytes())
+                   .compact();
+    }
+}

e9949baf28f92d7441ab9417dade828399ffb9d1
TTOzzi <devttozzi@gmail.com>
2020-06-11 15:25:49 +0900

    [iOS] feat: SignUpViewController 생성
    - 회원가입 화면을 담당 할 뷰 컨트롤러
    - 로그인 화면에서 SignUp 버튼을 누르면 modal 로 띄워짐
    
    #29

---
+++ iOS/IssueTracker/IssueTracker/SignUp/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  IssueTracker
+//
+//  Created by TTOzzi on 2020/06/11.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

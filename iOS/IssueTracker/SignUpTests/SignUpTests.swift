d2eb6d1b0aadddbac8ce41ddbed63edcbc25bb7a
TTOzzi <devttozzi@gmail.com>
2020-06-12 21:39:11 +0900

    [iOS] test: 테스트 코드 수정
    - 상세하게 케이스를 나눔
    
    close #47

--- iOS/IssueTracker/SignUpTests/SignUpTests.swift
+++ iOS/IssueTracker/SignUpTests/SignUpTests.swift
@@ -13,58 +13,110 @@ final class SignUpTests: XCTestCase {
     
     private var viewModel: SignUpViewModel!
     
+    func generateString(length: ClosedRange<Int>) -> String {
+        return (0..<Int.random(in: length)).map({ _ in "a" }).joined()
+    }
+    
     override func setUp() {
         super.setUp()
         viewModel = SignUpViewModel()
     }
     
     func testUserNameValid() {
-        viewModel.userName.value = "aaaaaaaa"
-        XCTAssertTrue(viewModel.isUserNameValid.value)
+        viewModel.signUpInfo.userName = generateString(length: 6...12)
+        do {
+            let userName = try XCTUnwrap(viewModel.isUserNameValid.value)
+            XCTAssertTrue(userName)
+        } catch {
+            XCTFail()
+        }
     }
     
-    func testUserNameInvalid() {
-        viewModel.userName.value = "aaa"
-        XCTAssertFalse(viewModel.isUserNameValid.value)
-        viewModel.userName.value = "aaaaaaaaaaaaaaa"
-        XCTAssertFalse(viewModel.isUserNameValid.value)
+    func testUserNameLengthUnder() {
+        viewModel.signUpInfo.userName = generateString(length: 0...5)
+        do {
+            let userName = try XCTUnwrap(viewModel.isUserNameValid.value)
+            XCTAssertFalse(userName)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testUserNameLengthOver() {
+        viewModel.signUpInfo.userName = generateString(length: 13...100)
+        do {
+            let userName = try XCTUnwrap(viewModel.isUserNameValid.value)
+            XCTAssertFalse(userName)
+        } catch {
+            XCTFail()
+        }
     }
     
     func testPasswordValid() {
-        viewModel.password.value = "aaaaaaaa"
-        XCTAssertTrue(viewModel.isPasswordValid.value)
+        viewModel.signUpInfo.password = generateString(length: 6...12)
+        do {
+            let password = try XCTUnwrap(viewModel.isPasswordValid.value)
+            XCTAssertTrue(password)
+        } catch {
+            XCTFail()
+        }
     }
     
-    func testPasswordInvalid() {
-        viewModel.password.value = "aaa"
-        XCTAssertFalse(viewModel.isPasswordValid.value)
-        viewModel.password.value = "aaaaaaaaaaaaaaa"
-        XCTAssertFalse(viewModel.isPasswordValid.value)
+    func testPasswordLengthUnder() {
+        viewModel.signUpInfo.password = generateString(length: 0...5)
+        do {
+            let password = try XCTUnwrap(viewModel.isPasswordValid.value)
+            XCTAssertFalse(password)
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testPasswordLengthOver() {
+        viewModel.signUpInfo.password = generateString(length: 13...100)
+        do {
+            let password = try XCTUnwrap(viewModel.isPasswordValid.value)
+            XCTAssertFalse(password)
+        } catch {
+            XCTFail()
+        }
     }
     
     func testConfirmPasswordValid() {
-        viewModel.password.value = "aaaaaaaa"
-        viewModel.confirmPassword.value = "aaaaaaaa"
-        XCTAssertTrue(viewModel.isConfirmPasswordValid.value)
+        viewModel.signUpInfo.password = "password"
+        viewModel.signUpInfo.confirmPassword = "password"
+        do {
+            let isValid = try XCTUnwrap(viewModel.isConfirmPasswordValid.value)
+            XCTAssertTrue(isValid)
+        } catch {
+            XCTFail()
+        }
     }
-    
+
     func testConfirmPasswordInvalid() {
-        viewModel.password.value = "aaaaaaaa"
-        viewModel.confirmPassword.value = "aaa"
-        XCTAssertFalse(viewModel.isConfirmPasswordValid.value)
+        viewModel.signUpInfo.password = "password1"
+        viewModel.signUpInfo.confirmPassword = "password2"
+        do {
+            let isValid = try XCTUnwrap(viewModel.isConfirmPasswordValid.value)
+            XCTAssertFalse(isValid)
+        } catch {
+            XCTFail()
+        }
     }
-    
+
     func testSignUpEnabled() {
         testUserNameValid()
         testPasswordValid()
         testConfirmPasswordValid()
         XCTAssertTrue(viewModel.isEnabled.value)
     }
-    
+
     func testSignUpForbid() {
-        testUserNameInvalid()
-        testPasswordValid()
-        testConfirmPasswordValid()
+        testUserNameLengthOver()
+        testUserNameLengthUnder()
+        testPasswordLengthOver()
+        testPasswordLengthUnder()
+        testConfirmPasswordInvalid()
         XCTAssertFalse(viewModel.isEnabled.value)
     }
 }

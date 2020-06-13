c910b222ccfca5b49dc47695d824c9c4f6b14089
1Consumption <hanseop95@gmail.com>
2020-06-13 21:02:00 +0900

    [iOS] feat: IssueTableView 연결
    - IssueTableView를 IssueListViewController와 연결
    - IssueStackView Bottom Anchor 적용안되던 오류 수정
    
    #41

--- iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class IssueListViewController: UIViewController {
     
+    @IBOutlet weak var IssueListTableView: UITableView!
+    
     private var searchController = UISearchController()
     
     override func viewDidLoad() {
         searchController.searchBar.placeholder = "Search"
         navigationItem.searchController = searchController
+        IssueListTableView.dataSource = self
+    }
+}
+
+extension IssueListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "IssueCell") else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }

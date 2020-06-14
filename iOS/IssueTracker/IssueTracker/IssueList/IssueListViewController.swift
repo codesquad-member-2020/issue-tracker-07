11941dd1c5f98efa5f2f5a6d11140a6a24fceb65
TTOzzi <devttozzi@gmail.com>
2020-06-14 21:53:33 +0900

    [iOS] feat: 이슈 목록 화면 셀 스와이프 액션 UI 구현
    - Delegate 메소드를 활용해 leading swipe 와 trailing swipe 에 액션을 줌
    
    close #42

--- iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
+++ iOS/IssueTracker/IssueTracker/IssueList/IssueListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class IssueListViewController: UIViewController {
     
-    @IBOutlet weak var IssueListTableView: UITableView!
+    @IBOutlet weak var issueListTableView: UITableView!
     
     private var searchController: UISearchController = UISearchController()
     private var dataSource: UITableViewDataSource = IssueTableViewDataSource()
@@ -18,6 +18,29 @@ class IssueListViewController: UIViewController {
     override func viewDidLoad() {
         searchController.searchBar.placeholder = "Search"
         navigationItem.searchController = searchController
-        IssueListTableView.dataSource = dataSource
+        issueListTableView.dataSource = dataSource
+        issueListTableView.delegate = self
+        
+    }
+}
+
+extension IssueListViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let closeAction = UIContextualAction(style: .normal, title: "Close",handler: { (_, _, _) in
+            
+        })
+        closeAction.backgroundColor = .systemGreen
+        
+        return UISwipeActionsConfiguration(actions: [closeAction])
+        
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete",handler: { (_, _, _) in
+
+        })
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }

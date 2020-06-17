f1566e4b639bad00b4dab81afade86a7a4f6496b
1Consumption <hanseop95@gmail.com>
2020-06-17 22:46:19 +0900

    [iOS] feat: appleSignIn useCase 구현
    - body에 email을 담아서 요청
    - 성공시 토큰을 핸들러로 넘겨줌
    
    #72

---
+++ iOS/IssueTracker/IssueTracker/Model/AppleLoginCertification.swift
@@ -0,0 +1,13 @@
+//
+//  AppleLoginCertification.swift
+//  IssueTracker
+//
+//  Created by 신한섭 on 2020/06/17.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct AppleLoginCertification: Codable {
+    var email: String
+}

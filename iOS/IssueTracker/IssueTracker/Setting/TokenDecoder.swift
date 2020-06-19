804c90f57224492bf769c23ca1856e9acf99b962
1Consumption <hanseop95@gmail.com>
2020-06-19 16:13:47 +0900

    [iOS] feat: tokenDecoder 추가
    - jwt 토큰의 payload를 디코드하는 객체 생성

---
+++ iOS/IssueTracker/IssueTracker/Setting/TokenDecoder.swift
@@ -0,0 +1,40 @@
+//
+//  TokenDecoder.swift
+//  IssueTracker
+//
+//  Created by 신한섭 on 2020/06/19.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct TokenDecoder {
+    static func decode(jwtToken jwt: String) -> [String: Any] {
+        let segments = jwt.components(separatedBy: ".")
+        return decodeJWTPart(segments[1]) ?? [:]
+    }
+    
+    static private func base64UrlDecode(_ value: String) -> Data? {
+        var base64 = value
+            .replacingOccurrences(of: "-", with: "+")
+            .replacingOccurrences(of: "_", with: "/")
+        
+        let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
+        let requiredLength = 4 * ceil(length / 4.0)
+        let paddingLength = requiredLength - length
+        if paddingLength > 0 {
+            let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
+            base64 = base64 + padding
+        }
+        return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
+    }
+    
+    static private func decodeJWTPart(_ value: String) -> [String: Any]? {
+        guard let bodyData = base64UrlDecode(value),
+            let json = try? JSONSerialization.jsonObject(with: bodyData, options: []), let payload = json as? [String: Any] else {
+                return nil
+        }
+        
+        return payload
+    }
+}

f6aaa83f9467a7b54e4addddee7a0b9f0afa76ad
TTOzzi <devttozzi@gmail.com>
2020-06-22 17:18:58 +0900

    [iOS] feat: 이슈 상세 화면 콜렉션 뷰 헤더, 셀 UI 적용
    - 이슈 상세화면의 셀을 화면에 표시
    
    #75

--- iOS/IssueTracker/IssueTracker/DetailIssue/DetailIssueViewController.swift
+++ iOS/IssueTracker/IssueTracker/DetailIssue/DetailIssueViewController.swift
@@ -10,13 +10,43 @@ import UIKit
 
 final class DetailIssueViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var issueDetailCollectionView: UICollectionView!
+    
     // MARK: - Properties
     static let identifier: String = "DetailIssue"
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        issueDetailCollectionView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         navigationController?.navigationBar.topItem?.title = ""
         navigationItem.setRightBarButton(UIBarButtonItem(title: "Edit", style: .plain, target: nil, action: nil), animated: true)
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationItem.largeTitleDisplayMode = .automatic
+    }
+}
+
+extension DetailIssueViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "contentCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "detailIssueHeader", for: indexPath)
+        return header
     }
 }

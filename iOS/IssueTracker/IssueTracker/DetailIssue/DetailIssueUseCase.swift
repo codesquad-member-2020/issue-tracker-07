9130363fb3d776a9894c6fe0e0e13f3c8ef30ad5
TTOzzi <devttozzi@gmail.com>
2020-06-23 17:37:36 +0900

    [iOS] refactor: 목 데이터 수정
    - JSON 데이터 수정
    - 모든 기능을 보여 줄 수 있도록 목데이터 수정
    
    close #85

--- iOS/IssueTracker/IssueTracker/DetailIssue/DetailIssueUseCase.swift
+++ iOS/IssueTracker/IssueTracker/DetailIssue/DetailIssueUseCase.swift
@@ -17,10 +17,10 @@ struct DetailIssueUseCase {
           "contents": [
             {
               "id": 1,
-              "title": "이슈 제목",
-              "description": "이슈 내용",
-              "authorName": "sedin",
-              "imageUrl": "https://codesquad-mocha.s3.ap-northeast-2.amazonaws.com/github.png",
+              "title": "이슈 상세 정보 화면 네트워킹",
+              "description": "Yeah 다시 돌아왔지 내 이름 레인(RAIN) 스웩을 뽐내 WHOO! They call it! 왕의 귀환 후배들 바빠지는 중! 신발끈 꽉 매고 스케줄 All Day 내 매니저 전화기는 조용할 일이 없네 WHOO!",
+              "authorName": "rain_oppa",
+              "imageUrl": "https://image.chosun.com/sitedata/image/202005/22/2020052201547_0.png",
               "isOpen": true,
               "createdAt": "2020-06-22 18:28:34",
               "modifiedAt": "2020-06-22 18:28:34",
@@ -56,7 +56,7 @@ struct DetailIssueUseCase {
               "id": 1,
               "writer": "mocha",
               "imageUrl": "https://codesquad-mocha.s3.ap-northeast-2.amazonaws.com/github.png",
-              "content": "댓글 내용",
+              "content": "댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용댓글 내용",
               "createdAt": "2020-06-22 18:28:34",
               "modifiedAt": "2020-06-22 18:28:34",
               "emoji": [

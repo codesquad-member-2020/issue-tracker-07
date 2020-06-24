c0907c223885acab4aee99b16d99d277c883da23
TTOzzi <devttozzi@gmail.com>
2020-06-24 17:57:24 +0900

    [iOS] feat: Label 커스텀 셀 구현
    - Label 정보를 표시 할 커스텀 셀 구현
    
    #97

---
+++ iOS/IssueTracker/IssueTracker/LabelList/LabelCell.swift
@@ -0,0 +1,14 @@
+//
+//  LabelCell.swift
+//  IssueTracker
+//
+//  Created by TTOzzi on 2020/06/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class LabelCell: UITableViewCell {
+    static let identifier: String = "labelCell"
+}
+

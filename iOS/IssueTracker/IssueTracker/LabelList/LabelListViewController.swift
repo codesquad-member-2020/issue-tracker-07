ba51b4ad406670846f1622c52902dbeb9d3650f3
TTOzzi <devttozzi@gmail.com>
2020-06-24 16:55:23 +0900

    [iOS] feat: 레이블 관리 화면 테이블 뷰 설정
    - 레이블 리스트를 담을 테이블 뷰 생성
    - 프로토 타입 셀 생성
    - 셀에 악세사리뷰로 “>”를 표시한다.
    
    close #96

---
+++ iOS/IssueTracker/IssueTracker/LabelList/LabelListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LabelListViewController.swift
+//  IssueTracker
+//
+//  Created by TTOzzi on 2020/06/24.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class LabelListViewController: UIViewController {
+
+}
